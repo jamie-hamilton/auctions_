44fc19e6f60a5390afc093dd7a1579c133e645b6
Jamie Hamilton <jhamilton.contact@gmail.com>
2020-09-16 17:29:56 +0100

    :keyboard: add static urls to urls.py

--- commerce/urls.py
+++ commerce/urls.py
@@ -19,4 +19,4 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
